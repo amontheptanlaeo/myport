fa7d5c9b464fd46efe13b176f12028444985f05c
KuroMuta <legiony530@gmail.com>
2022-04-25 02:21:53 +0700

    finished

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,19 +51,19 @@ function Navbar({mySkill , myAbout , myContact , myHome}) {
                 <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
             </svg>
         </motion.button>
-        <div className={`absolute ${isOpen ? 'right-0 fixed':'right-[-1000px]'} sm:hidden w-full top-0 bg-[#8400ff] h-screen flex justify-center items-center flex-col gap-10 transition-all duration-300 ease-linear`}>
-            <div className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#591c92]' onClick={()=>setIsOpen(!isOpen)}>X</div>
-            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#591c92]' onClick={() => {
+        <div className={`absolute ${isOpen ? 'right-0 fixed':'right-[-1000px]'} sm:hidden w-full top-0 bg-[#E4778C] border-l-8 border-white h-screen flex justify-center items-center flex-col gap-10 transition-all duration-300 ease-linear`}>
+            <div className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#000000] text-[#000000]' onClick={()=>setIsOpen(!isOpen)}>X</div>
+            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#000000] text-[#000000]' onClick={() => {
               myHome.current.scrollIntoView() 
               setIsOpen(!isOpen)}} >Home</p>
-            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#591c92]' onClick={() => {
+            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#000000] text-[#000000]' onClick={() => {
               mySkill.current.scrollIntoView()
               setIsOpen(!isOpen)}}>Skill</p>
-            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#591c92]'onClick={() =>{ 
+            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#000000] text-[#000000]'onClick={() =>{ 
               myAbout.current.scrollIntoView()
               setIsOpen(!isOpen)
               }}>Certificate</p>
-            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#591c92]'onClick={() => {
+            <p className='cursor-pointer transition-all duration-100 ease-linear hover:text-[#000000] text-[#000000]'onClick={() => {
               myContact.current.scrollIntoView()
               setIsOpen(!isOpen)
               }}>About</p>
@@ -71,10 +71,11 @@ function Navbar({mySkill , myAbout , myContact , myHome}) {
         <motion.ul variants={container}
             initial="hidden"
             animate="visible" className='gap-6 hidden sm:flex'>
-            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#8400ff] hover:border-[#8400ff] text-[16px] md:text-[20px]' onClick={() => myHome.current.scrollIntoView()}>Home</motion.li>
-            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#8400ff] hover:border-[#8400ff] text-[16px] md:text-[20px]'  onClick={() => mySkill.current.scrollIntoView() }>Skill</motion.li>
-            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#8400ff] hover:border-[#8400ff] text-[16px] md:text-[20px]' onClick={() => myAbout.current.scrollIntoView()}>Certificate</motion.li>
-            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#8400ff] hover:border-[#8400ff] text-[16px] md:text-[20px]'onClick={() => myContact.current.scrollIntoView()}>About</motion.li>
+            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#64C1ED] hover:border-[#64C1ED] text-[16px] md:text-[20px]' onClick={() => myHome.current.scrollIntoView()}>Home</motion.li>
+            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#E4778C] hover:border-[#E4778C] text-[16px] md:text-[20px]'  onClick={() => mySkill.current.scrollIntoView() }>Skill</motion.li>
+            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#7CE4BB] hover:border-[#7CE4BB] text-[16px] md:text-[20px]' onClick={() => myAbout.current.scrollIntoView()}>Certificate</motion.li>
+            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#EBCD51] hover:border-[#EBCD51] text-[16px] md:text-[20px]'onClick={() => myContact.current.scrollIntoView()}>About</motion.li>
+            <motion.li variants={item} className='cursor-pointer p-3 hover:border-b-2 transition-all duration-100 ease-linear hover:py-2 hover:text-[#A39EEF] hover:border-[#A39EEF] text-[16px] md:text-[20px]'onClick={() => myContact.current.scrollIntoView()}>Github</motion.li>
         </motion.ul>
     </div>
   )
